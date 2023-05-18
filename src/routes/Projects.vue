298576e406a5befb587bd62a278cd8227456b879
jinach0i <80081194+jinach0i@users.noreply.github.com>
2023-05-18 21:13:35 +0900

    update Projects.vue

--- src/routes/Projects.vue
+++ src/routes/Projects.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        
+ef="https://jinach0i.github.io/CC_NHIS/" 
+i>
+                            <a href="https://jinach0i.github.io/CC-SKChemicals/"
     </div>
 </template>
 <script>
