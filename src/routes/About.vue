721ac5ad5d74594fd6ad6b844488a0f4b7179d05
jinach0i <80081194+jinach0i@users.noreply.github.com>
2023-05-18 21:12:28 +0900

    update About.vue

--- src/routes/About.vue
+++ src/routes/About.vue
@@ -1,6 +1,31 @@
 <template>
     <div>
-        
+
+                                <h3>안녕하세요~ 웹 퍼블리셔에 지원한 최혜진입니다. &#x1F604;</h3>
+                                <strong>출생년도</strong>
+                                <p>1993.01.09</p>
+                                <strong>교육</strong>
+                                <span>
+                                    <p>용인대학교 중국학과</p>
+                                    <p>조성인터내셔널 무역파트</p>
+                                    <p>더휴먼아카데미 웹퍼블리셔프론트엔드 608시간 과정 수료</p>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="whatbenefitswhenworkwithme">
+                            <h4>저의 3가지 특징은 아래와 같습니다.:)</h4>
+                            <ul>
+                                <li><strong>긍정적</strong>
+                                    <p>안되도 잠시 안되는것일뿐, 곧 되게 만들면 된다고 생각합니다</p>
+                                </li>
+                                <li><strong>끈기</strong>
+                                    <p>포기란 배추를 셀 때 쓰는 단위죠</p>
+                                </li>
+                                <li><strong>고객지향적</strong>
+                                    <p>작업의 궁극적 목표는 '고객입장에서 회사에게'라고 생각합니다</p>
+                                </li>
+                            </ul>
+                        </div>
     </div>
 </template>
 <script>
