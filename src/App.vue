3b0ee382679110c3f94d735a268669cece7fe6f2
jinach0i <ginach0irk@gmail.com>
2023-05-18 11:27:16 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Header from './components/Header.vue';
+import Container from './components/Container.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <div id="App">
+    <Header />
+    <Container />
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="container">
+        
+    </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+    
+</style>
